145f92c9774b3a237a9cb2f0ef02503c2025ed89
RastafariHabbo <rastafarihabbobr@gmail.com>
2025-05-02 00:38:32 -0300

    Lista de notícias oficiais

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,28 @@ import FeaturedNews from '../components/FeaturedNews';
 import NewsGrid from '../components/NewsGrid';
 import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css';
+import NewsHabboList from '../components/NewsHabboList/NewsHabboList';
 
 export default function Home() {
   return (
     <Layout withBackground>
       <Head>
-        <title>Fundo Lotus - Página Inicial</title>
+        <title>Lotus Habbo - Página Inicial</title>
         <meta name="description" content="Bem-vindo a Lotus Habbo" />
       </Head>
 
       <div className={styles.container}>
         <FeaturedNews />
-        <div className={styles.backgroundgrid}>
-          <NewsGrid />
+        <div className={styles.contentSplit}>
+          <div className={styles.backgroundgrid}>
+            <NewsGrid />
+          </div>
+          <div className={styles.newsSide}>
+            <div className={styles.newsSideHeader}>Notícias Oficiais</div>
+            <NewsHabboList />
+          </div>
         </div>
       </div>
     </Layout>
   );
-} 
+}
